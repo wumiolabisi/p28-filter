a9ca2205bb0bfc0c02e0a91d925565bc5c0bb8c0
unknown <wumi.olabisi@gmail.com>
2024-12-04 14:49:40 +0100

    Tests de la fonction p28_get_caracteristics OK

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 require dirname(__FILE__) . '/../vendor/autoload.php';
 
-define('ABSPATH', dirname(__FILE__) . '/../../../../');
+define('ABSPATH', dirname(__FILE__) . '/../');
 
-require_once ABSPATH . 'wp-load.php';
+//require_once ABSPATH . 'wp-load.php';
+if (!function_exists('plugin_dir_path')) {
+    function plugin_dir_path($file)
+    {
+        return ABSPATH;
+    }
+}
